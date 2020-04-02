b708b426a739d6ecfee77b0689aa3cea4d5304fa
trancept <benoit@nowhere.com>
2020-04-02 09:29:17 +0200

    Adding wind speed

--- pipeline/extract_nc4_to_csv.py
+++ pipeline/extract_nc4_to_csv.py
@@ -12,64 +12,49 @@ output_dir = r'../../../datasets/OCO2/csv/'
 dir = os.path.realpath(input_dir)
 print(dir)
 
+years_months = []
+for year in range(14, 20+1):
+    for month in range(1,12+1):
+        years_months.append(str(year)+str(month).zfill(2))
+
+#print(years_months)
 # Loop over years from 2014
-for year in range(14, 20):
-    year = str(year)
-    print('\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n')
-    print('Switch to year', year)
-
+for year_month in years_months:
+    #year = str(year)
+    print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    print('Switch to year/month', year_month)
     # Get the file list in directory
-    nc4_list = glob.glob(input_dir + "oco2_LtCO2_"+year+"*.nc4")
-
-    # Initialize array to store data
-    sounding_array = []
-    latitude_array = []
-    longitude_array = []
-    xco2_array = []
-    xco2_uncert_array = []
-    orbit_array = []
-
+    nc4_list = glob.glob(input_dir + "oco2_LtCO2_"+year_month+"*.nc4")
+    # # Initialize array to store data
+    month_data = np.empty((0,9))
     # Loop over the files
     for one_file in nc4_list:
-        print(one_file)
-
+        print('Reading', one_file)
         # Open the file
         try:
             file_nc = Dataset(one_file, 'r')
         except:
             print('ERROR reading', one_file)
             continue
-        # Extract information
-        latitude = file_nc.variables['latitude'][:]
-        longitude = file_nc.variables['longitude'][:]
-        sounding_id = file_nc.variables['sounding_id'][:]  # YYYYMMDDhhmmssmf
-        orbit = file_nc.groups['Sounding'].variables['orbit'][:]
-        # height = file_nc.groups['Sounding'].variables['altitude'][:]
-        xco2 = file_nc.variables['xco2'][:]     #ppm
-        flag = file_nc.variables['xco2_quality_flag'][:] # 0=Good, 1=Bad
-        xco2_uncert = file_nc.variables['xco2_uncertainty'][:]
-
-        # Filter the data to remove bad quality data
-        for index in range( len(latitude) ):
-            # Don't read bad data
-            if flag[index] :
-                # Skip storing for bad data
-                continue
-            # Store good data
-            sounding_array.append(sounding_id[index])
-            latitude_array.append(latitude[index])
-            longitude_array.append(longitude[index])
-            xco2_array.append(xco2[index])
-            xco2_uncert_array.append(xco2_uncert[index])
-            orbit_array.append(orbit[index])
+        np_table = np.column_stack((file_nc.variables['xco2_quality_flag'],file_nc.variables['sounding_id'],file_nc.variables['latitude'],file_nc.variables['longitude'],
+            file_nc.variables['xco2'],file_nc.variables['xco2_uncertainty'],file_nc.groups['Sounding'].variables['orbit'], file_nc.groups['Meteorology']['windspeed_u_met'], file_nc.groups['Meteorology']['windspeed_v_met']))
+        month_data = np.concatenate((month_data, np_table), axis=0)
+    if(month_data.size == 0):
+        continue
     # Save this year to disk
-    print("End of", year, 'saving to disk...')
-    np_table = np.column_stack((sounding_array,latitude_array,longitude_array,xco2_array,xco2_uncert_array,orbit_array))
-    df = pd.DataFrame(np_table, columns=['sounding_id', 'latitude', 'longitude', 'xco2', 'xco2_uncert', 'orbit'])
+    print("End of", year_month, 'preparing dataframe...')
+
+    df = pd.DataFrame(month_data, columns=['flag','sounding_id', 'latitude', 'longitude', 'xco2', 'xco2_uncert', 'orbit', 'windspeed_u', 'windspeed_v'])
     # using dictionary to convert specific columns (https://www.geeksforgeeks.org/change-data-type-for-one-or-more-columns-in-pandas-dataframe/)
     convert_dict = {'sounding_id': int, 
                     'orbit': int
                 } 
     df = df.astype(convert_dict) 
-    df.to_csv(output_dir + 'oco2_20'+year+'.csv', sep=';', index=False)
+    # Remove bad quality
+    df=df[df['flag']==0]
+    # Remove flag
+    df.drop(['flag'], axis=1, inplace=True)
+    print('Saving to disk...')
+    df.to_csv(output_dir + 'oco2_'+year_month+'.csv', sep=';', index=False)
     del df
+    #break
