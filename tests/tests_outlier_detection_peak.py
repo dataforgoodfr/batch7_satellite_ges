c886b8c0d202f64c4e58506b08375bbdf6a84be5
raphaeleadjerad <raphaele.adjerad@gmail.com>
2020-05-03 13:52:57 +0200

    outlier detection test

--- tests/tests_outlier_detection_peak.py
+++ tests/tests_outlier_detection_peak.py
@@ -2,16 +2,20 @@ import pipeline.outlier_detection_peak as out_detect
 import pandas as pd
 import numpy as np
 
+# Test on one day outlier detection
 # TODO: test this detection without parameter implement_filters set to False in gaussian_fit_on_df
 # TODO: implement function design_features to add features to LOF based on Charlotte Delgot notebook
 data_1808_25 = out_detect.extract_orbit_date("dataset/oco2_1808.csv", 25)
 print(data_1808_25.shape)
 df_full = out_detect.compute_distance(data_1808_25)
 peaks_found = out_detect.gaussian_fit_on_df(df_full, input_name='', output_dir='', output_peak=False, output_csv=False,
-                                            implement_filters=False)
+                                            implement_filters=True)
 peaks = pd.DataFrame(peaks_found)
+
 print(peaks.shape)  # (108, 12) avec filters, (306,12) sans filter
 peaks = out_detect.detect_outliers_lof(peaks, neighbors=10, features=["latitude", "longitude", "slope", "intercept",
                                                                       "amplitude", "sigma", "delta", "R"])
 np.random.seed(18)
 out_detect.compare_peaks(df_full, peaks)
+
+# Test on all peaks of a month outlier detection and create csv with classif
