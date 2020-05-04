3919bca24475d96ba570312fe027c8f5bcfb5107
trancept <ben@benemie.fr>
2020-05-04 08:58:08 +0200

    Streamlit PyDeck from Swift Object

--- pipeline/streamlit_pydeck.py
+++ pipeline/streamlit_pydeck.py
@@ -2,10 +2,36 @@ import streamlit as st
 import pandas as pd
 import pydeck as pdk
 from pydeck import data_utils
+import swiftclient
+import json
+import re
 
-files = {"2017": "dataset/CO2_emissions_Edgar_2017_v3.csv",
-         "2018": "dataset/CO2_emissions_Edgar_2018_v3.csv"}
 
+# Read config file
+with open("../config.json") as json_data_file:
+    config = json.load(json_data_file)
+
+def swift_con(config):
+    """
+    Function to connect to Open Stack Swift Object Storage
+    :param config: JSON configuration
+    :return: swiftclient.Connection
+    """
+    return swiftclient.Connection(user=config['swift_storage']['user'],key=config['swift_storage']['key'], authurl=config['swift_storage']['auth_url'],os_options=config['swift_storage']['options'],tenant_name=config['swift_storage']['tenant_name'], auth_version=config['swift_storage']['auth_version'])
+
+# connect to Open Stack Swift Object Storage
+swift_conn = swift_con(config)
+
+# Retrieve file list
+files = {}
+objects = swift_conn.get_container('oco2')[1]
+for data in objects:
+    if 'CO2_emissions_Edgar' in data['name']:
+        #print('{0}\t{1}\t{2}'.format(data['name'], data['bytes'], data['last_modified']))
+        year = d = re.findall(r'(\d{4})', data['name'])[-1]
+        files.update(
+            {year : config['swift_storage']['base_url']+data['name']}
+        )
 
 @st.cache
 def load_data(file):
@@ -17,10 +43,12 @@ def load_data(file):
     co2["co2em"] = round(co2["co2em"])
     return co2
 
+"""
+# EDGAR CO2 levels
+This map shows the CO² levels from [EDGAR](https://edgar.jrc.ec.europa.eu/overview.php).
+"""
 
-st.title("EDGAR CO2 levels")
-year = st.sidebar.selectbox("Year", ('2017', '2018'))
-
+year = st.sidebar.selectbox("Year", tuple(files.keys()) )
 
 df = load_data(files[year])
 view = data_utils.compute_view(df[["longitude", "latitude"]])
@@ -28,7 +56,6 @@ view.pitch = 50
 view.bearing = 0
 view.zoom = 1
 view.max_zoom = 5
-
 
 tooltip = {
    "html": "<b>CO2 level x 10*8:</b> {co2em}",
